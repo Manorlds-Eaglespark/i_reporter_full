6650b4c45d66cad15817da4019565a83399a050d
Anorld Mukone <manorldsapiens@gmail.com>
2019-02-09 13:57:18 +0300

    [#163842197] edit css improvements

--- js/functions.js
+++ js/functions.js
@@ -7,9 +7,9 @@ function login_user() {
     }
     let request = new Request('http://127.0.0.1:5000/api/v2/auth/login', {
         method: "POST",
-        headers: {
+        headers: new Headers({
             "Content-Type": "application/json"
-        },
+        }),
         body: JSON.stringify(data_),
         mode:'cors'
     });
